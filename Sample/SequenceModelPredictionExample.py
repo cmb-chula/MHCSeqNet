043da2c4d74ab1e9bc38071b1e1e186bf35e26c2
cmbcu <cmb.chula@gmail.com>
2019-01-22 14:10:28 +0900

    Update default path to pretrained model
    To allow user to test this script right from the cloned MHCSeqNet folder

--- Sample/SequenceModelPredictionExample.py
+++ Sample/SequenceModelPredictionExample.py
@@ -12,7 +12,7 @@ data = np.array([['TYIGSLPGK','HLA-B*58:01'],
 bindingSequencePredictor = BindingSequencePredictor()
 
 # Load model from path. Please replace [path to project] to the path to the root of this project in your machine.
-bindingSequencePredictor.load_model('[path to project]/Neoantigen-Predictor/PredictionModel/Pretrained Models/sequence_model/')
+bindingSequencePredictor.load_model('./MHCSeqNet/PredictionModel/Pretrained Models/sequence_model/')
 
 # Predict binding probability in the pair of a peptide and an MHC allele.
 result = bindingSequencePredictor.predict(data[:, 0],
