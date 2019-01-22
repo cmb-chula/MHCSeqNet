a2a700ff2806b940d5cd7d7b52327ed2cb6681d8
cmbcu <cmb.chula@gmail.com>
2019-01-22 14:02:21 +0900

    Update version number and details

--- Setup.py
+++ Setup.py
@@ -9,13 +9,13 @@ import setuptools
 
 setup(
     name="MHCSeqNet",
-    version="0.1",
+    version="1.0",
     #author="Example Author",
     #author_email="author@example.com",
-    #description="A small example package",
+    description="MHC ligand prediction tool",
     #long_description=long_description,
     #long_description_content_type="text/markdown",
-    #url="https://github.com/pypa/example-project",
+    url="https://github.com/cmbcu/MHCSeqNet/",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
